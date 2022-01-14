404c1602015b927ab6e46f608bed2adf31951515
sstby <91950311+sstby@users.noreply.github.com>
2022-01-14 17:16:17 +0200

    Shoplist save

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -1,54 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { database, auth } from "../firebase";
+import { doc, getDoc, collection, addDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
-const Shoplist = props => {
-    const shoplistItems = Object.keys(props.shoplist)
+const Shoplist = (props) => {
+  const shoplistItems = props.shoplist;
+  /* const [shoplistItems, setShoplistItems] = useState({});
 
-    const handleChange = (event) => {
-        const updatedItem = props.shoplist[event.currentTarget.name].ingridient;
-        const updatedCount = parseFloat(event.currentTarget.value) || '';
-        props.updateShopList(updatedItem, updatedCount)
-        console.log(updatedItem,updatedCount)
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      user && syncShoplist(user.email);
+    });
+    console.log("hi");
+  }, []);
+
+  const syncShoplist = async (mail) => {
+    const shoplistRef = doc(database, "Shoplists", mail);
+    const shoplistSnap = await getDoc(shoplistRef);
+    if (shoplistSnap.exists()) {
+      const data = { ...shoplistSnap.data() };
+      setShoplistItems(data);
     }
+  }; */
 
-    const renderShoplist = (key) => {
-        const item = props.shoplist[key];
-        return (
-            <tr key={key}>
-                <td>{item.ingridient}:</td>
-                <td>
-                    <input 
-                    name={key}
-                    type='text' 
-                    value={item.count} 
-                    onChange={handleChange}
+  const handleChange = (event) => {
+    const updatedItem = props.shoplist[event.currentTarget.name].ingridient;
+    const updatedCount = parseFloat(event.currentTarget.value) || "";
+    props.updateShopList(updatedItem, updatedCount);
+  };
 
-                    />
-
-
-                </td>
-                <td>{item.measure}</td>
-                <td >
-                    <button 
-                    onClick={() => props.removeFromShoplist(key)}
-                    className='cancellItem'>
-                        &times;
-                    </button>
-                </td>
-            </tr>
-        )
-    }
-
+  const renderShoplist = (key) => {
+    const item = shoplistItems[key];
     return (
-            <div className='shoplist'>
-                <h1>Shoplist</h1>
-                <table className='shoplist-items'>
-                    <tbody>
-                        {shoplistItems.map(renderShoplist)}
-                    </tbody>
-                </table>
-            </div>
-        
-    )
-}
+      <tr key={key}>
+        <td>{item.ingridient}:</td>
+        <td>
+          <input
+            name={key}
+            type="text"
+            value={item.count}
+            onChange={handleChange}
+          />
+        </td>
+        <td>{item.measure}</td>
+        <td>
+          <button
+            onClick={() => props.removeFromShoplist(key)}
+            className="cancellItem"
+          >
+            &times;
+          </button>
+        </td>
+      </tr>
+    );
+  };
 
-export default Shoplist
+  return (
+    <div className="shoplist">
+      <h1>Shoplist</h1>
+      <table className="shoplist-items">
+        <tbody>{Object.keys(shoplistItems).map(renderShoplist)}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Shoplist;
--- src/context/MyContext.js
+++ src/context/MyContext.js
@@ -7,26 +7,43 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
 } from "firebase/auth";
-import { auth } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, database } from "../firebase";
 
-const MyContext = createContext();
+export const MyContext = createContext();
 
 export function MyContextProvider({ children }) {
   //Всё, что связано с авторизацией пользователя
   const [user, setUser] = useState({});
 
+  function createUserProfile(registeredUser) {
+    console.log(registeredUser);
+    const name = registeredUser.displayName;
+    const email = registeredUser.email;
+
+    setDoc(doc(database, "Users", email), {
+      name: name,
+      email: email,
+    });
+  }
+
   function logIn(email, password) {
     return signInWithEmailAndPassword(auth, email, password);
   }
   function signUp(email, password) {
-    return createUserWithEmailAndPassword(auth, email, password);
+    createUserWithEmailAndPassword(auth, email, password).then((snap) =>
+      createUserProfile(snap.user)
+    );
   }
   function logOut() {
     return signOut(auth);
   }
   function googleSignIn() {
     const googleAuthProvider = new GoogleAuthProvider();
-    return signInWithPopup(auth, googleAuthProvider);
+    signInWithPopup(auth, googleAuthProvider).then((snap) => {
+      const meta = snap.user.metadata;
+      meta.lastSignInTime === meta.creationTime && createUserProfile(snap.user);
+    });
   }
 
   useEffect(() => {
@@ -40,7 +57,9 @@ export function MyContextProvider({ children }) {
   }, []);
 
   return (
-    <MyContext.Provider value={{ user, logIn, signUp, logOut, googleSignIn }}>
+    <MyContext.Provider
+      value={{ user, logIn, signUp, logOut, googleSignIn, createUserProfile }}
+    >
       {children}
     </MyContext.Provider>
   );
